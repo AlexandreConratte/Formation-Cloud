448a7af0ab3494383d5bf356accc237765ca99fe
Alexandre C <conrattealex@gmail.com>
2026-07-08 16:14:08 +0200

    exo python 1 et 2

---
+++ python-algo-scripts/exercices/exo-02.py
@@ -0,0 +1,30 @@
+produits = ["coca", "fanta", "sprite", "orangina", "ice tea", "pomme", "poire"]
+price = [1.5, 1.4, 2.0, 1.6, 1.7, 0.3, 0.4]
+somme_totale = 20
+list_courses = []
+
+for i in range(len(produits)):
+    print(f"{produits[i]} : {price[i]}€")
+
+#for i in range(len(produits)):
+ #   select = input(f"Voulez-vous acheter le produit suivant :{produits[i]} ? (oui/non) ")
+  #  if select.lower() == "oui":
+   #    somme_totale -= price[i]
+    #   print(somme_totale)
+
+for i in range(len(produits)):
+    select = input(f"Voulez-vous acheter le produit suivant :{produits[i]} ? (oui/non) ")
+    if select.lower() == "oui":
+     somme_totale -= price[i] and list_courses.append(produits[i])
+    print(somme_totale)
+    
+somme_dépensée = 20-somme_totale
+
+print(f"Vous avez acheté {', '.join(list_courses)} pour {somme_dépensée}€ et il vous reste {somme_totale}€")
+
+
+
+ 
+
+
+
---
+++ python-algo-scripts/variables.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+mon_texte = "test"
+mon_nombre = 29
+mon_nombre_virgule = 29,9
+boolean = True
+
+print ("I'm a string")
+print (mon_texte)
+print (mon_nombre)                          
+print (mon_nombre_virgule)
+print (boolean)
+
+prenom = input("Your name:")
+print(prenom)
+
+
